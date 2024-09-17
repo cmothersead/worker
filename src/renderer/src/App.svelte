cc5914148262ca97060a79edf317e074120329e3
Colin Mothersead <colin@mysplit.app>
2024-09-17 01:23:24 -0400

    Initial commit

---
+++ src/renderer/src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<div class="container mx-auto my-10">
+  <div class="text-center text-3xl">Welcome to Colin's Awesome <i>Do Work</i> Tool!</div>
+  <div class="text-center text-lg">Let's do some work!</div>
+
+  <button class="outline outline-1 rounded p-2">Lane to Lanes</button>
+</div>
